e9159440b666945de32e4a12f677bbc4543f2df2
Irakliy Khaburzaniya <irakliyk@fb.com>
2021-03-04 19:50:15 -0800

    removed plasma and stream crates from the repo

--- stream/src/utils.rs
+++
@@ -1,7 +0,0 @@
-/// Converts a list of 20-byte arrays into plasma store object IDs.
-pub fn map_object_ids(object_ids: &[crate::ObjectId]) -> Vec<plasma::ObjectId> {
-    object_ids
-        .iter()
-        .map(|oid| plasma::ObjectId::new(*oid))
-        .collect()
-}
