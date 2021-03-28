34e185fd0a6a9df74a53d33f8e9f00b1c468020b
Irakliy Khaburzaniya <irakliyk@fb.com>
2021-03-28 00:53:28 -0700

    refactored constraint evaluation in the verifier

--- verifier/src/constraints.rs
+++ verifier/src/constraints.rs
@@ -1,6 +1,6 @@
 use common::{
-    AssertionConstraintGroup, CompositionCoefficients, ConstraintEvaluator, EvaluationFrame,
-    TransitionEvaluator,
+    AssertionConstraintGroup, Assertions, CompositionCoefficients, ComputationContext,
+    ConstraintDivisor, EvaluationFrame, PublicCoin, TransitionEvaluator,
 };
 use math::field::{BaseElement, FieldElement, FromVec};
 
@@ -8,50 +8,60 @@ use math::field::{BaseElement, FieldElement, FromVec};
 // ================================================================================================
 
 /// Evaluates constraints for the specified frame.
-pub fn evaluate_constraints<T: TransitionEvaluator, E: FieldElement + FromVec<BaseElement>>(
-    mut evaluator: ConstraintEvaluator<T>,
+pub fn evaluate_constraints<C, T, E>(
+    coin: &C,
+    context: &ComputationContext,
+    assertions: Assertions,
     ood_frame: &EvaluationFrame<E>,
     x: E,
-) -> E {
-    let mut result = E::ZERO;
+) -> E
+where
+    C: PublicCoin,
+    T: TransitionEvaluator,
+    E: FieldElement + FromVec<BaseElement>,
+{
+    // ----- evaluate transition constraints ------------------------------------------------------
 
-    // evaluate transition constraints
-    let evaluations = evaluator.evaluate_at_x(&ood_frame.current, &ood_frame.next, x);
-    let divisors = evaluator.constraint_divisors();
-    debug_assert!(
-        divisors.len() == evaluations.len(),
-        "number of divisors ({}) does not match the number of evaluations ({})",
-        divisors.len(),
-        evaluations.len()
-    );
+    // build the divisor for transition constraints; divisors for all transition constraints are
+    // the same and hav the form: (x^steps - 1) / (x - x_at_last_step)
+    let t_divisor = ConstraintDivisor::from_transition(context);
 
-    // iterate over evaluations and divide out values implied by the divisors
-    for (&evaluation, divisor) in evaluations.iter().zip(divisors.iter()) {
-        let z = divisor.evaluate_at(x);
-        result = result + evaluation / z;
-    }
+    // instantiate transition evaluator
+    let t_evaluator = T::new(context, coin.get_transition_coefficient_prng());
+    // evaluate transition constraints and merge them into a single value
+    let mut t_evaluations = vec![E::ZERO; t_evaluator.num_constraints()];
+    t_evaluator.evaluate_at_x(&mut t_evaluations, &ood_frame.current, &ood_frame.next, x);
+    let t_evaluation = t_evaluator.merge_evaluations(&t_evaluations, x);
 
-    // evaluate assertion constraints
-    let assertion_groups = evaluator.assertion_constraints();
-    if !assertion_groups.is_empty() {
-        let mut degree_adjustment = assertion_groups[0].degree_adjustment();
-        let mut xp = E::exp(x, degree_adjustment.into());
+    // divide out the evaluation of divisor at x
+    let z = t_divisor.evaluate_at(x);
+    let mut result = t_evaluation / z;
 
-        // iterate over assertion constraint groups (each group has a distinct divisor), evaluate
-        // constraints in each group and add them to the evaluations vector
-        for group in assertion_groups.iter() {
-            // if adjustment degree hasn't changed, no need to recompute `xp` - so just reuse the
-            // previous value; otherwise, compute new `xp`
-            if group.degree_adjustment() != degree_adjustment {
-                degree_adjustment = group.degree_adjustment();
-                xp = E::exp(x, degree_adjustment.into());
-            }
-            // evaluate all constraints in the group, and the divide out the value implied
-            // by the divisor
-            let evaluation = evaluate_assertion_group(group, &ood_frame.current, x, xp);
-            let z = group.divisor().evaluate_at(x);
-            result = result + evaluation / z;
+    // ----- evaluate assertion constraints -------------------------------------------------------
+
+    // convert assertions into assertion constraints grouped by common divisor
+    let assertion_groups =
+        assertions.into_constraints(context, coin.get_assertion_coefficient_prng());
+
+    // iterate over assertion constraint groups (each group has a distinct divisor), evaluate
+    // constraints in each group and add them to the evaluations vector
+
+    // cache power of x here so that we only re-compute it when degree_adjustment changes
+    let mut degree_adjustment = assertion_groups[0].degree_adjustment();
+    let mut xp = x.exp(degree_adjustment.into());
+
+    for group in assertion_groups.iter() {
+        // if adjustment degree hasn't changed, no need to recompute `xp` - so just reuse the
+        // previous value; otherwise, compute new `xp`
+        if group.degree_adjustment() != degree_adjustment {
+            degree_adjustment = group.degree_adjustment();
+            xp = x.exp(degree_adjustment.into());
         }
+        // evaluate all constraints in the group, and the divide out the value implied
+        // by the divisor
+        let evaluation = evaluate_assertion_group(group, &ood_frame.current, x, xp);
+        let z = group.divisor().evaluate_at(x);
+        result = result + evaluation / z;
     }
 
     result
